a64256fa51d289e9f93b3ba3199def51d213d486
Martin Fesneau <martin.fesneau@gmail.com>
2021-01-14 21:56:54 +0100

    add close button to help container

--- app/javascript/components/show_help.js
+++ app/javascript/components/show_help.js
@@ -1,8 +1,13 @@
 const showHelp = () => {
   const helpCont = document.querySelector(".help-cont");
   const helpButton = document.querySelector("#help-link");
+  const closeButton = document.querySelector(".help-cont .fa-times")
+  console.log(closeButton)
 
   helpButton.addEventListener("click", () => {
+    helpCont.classList.toggle("d-none");
+  });
+  closeButton.addEventListener("click", () => {
     helpCont.classList.toggle("d-none");
   });
 };
