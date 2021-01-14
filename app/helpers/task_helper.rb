bfd7446a3075efc87f17a82a7f8287c9962bb4ed
Martin Fesneau <martin.fesneau@gmail.com>
2021-01-14 18:33:49 +0100

    change priority to word in view

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -3,4 +3,15 @@ module TaskHelper
   def order_by_date(comments)
     comments.order(created_at: :asc)
   end
+
+  def priority_in_word(priority)
+    case priority
+      when 1
+        "High"
+      when 2
+        "Medium"
+      when 3
+        "Low"
+    end
+  end
 end
